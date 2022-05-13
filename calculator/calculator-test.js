61df31cedac32973f4a6b962b2077506cb837cba
Rose <rosepowell2001@gmail.com>
2022-05-13 13:39:00 -0400

    Part 1, Loan Calculator

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,85 @@
 
+
+
+describe("calculate Monthly Payment tests", function() {
+
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values ={
+    amount: ("10000"),
+    years: ("8"),
+    rate: ("5.8"),
+  };
+
+  expect(calculateMonthlyPayment(values)).toEqual("130.44");
+
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+const values ={
+  amount: ("10043"),
+  years: ("8"),
+  rate: ("5.8"),
+};
+
+expect(calculateMonthlyPayment(values)).toEqual('131.00');
+
 });
 
-/// etc
+
+it("should return a string", function() {
+const values ={
+  amount: ("10043"),
+  years: ("8"),
+  rate: ("5.8"),
+};
+
+expect(calculateMonthlyPayment(values)).toEqual(jasmine.any(String));
+
+});
+
+it("should still work with extra chars like $", function() {
+const values ={
+  amount: ("$10era04a3"),
+  years: ("8dfg"),
+  rate: ("5.sderga8"),
+};
+
+expect(calculateMonthlyPayment(values)).toEqual('131.00');
+
+});
+
+
+});
+
+
+beforeEach(function() {
+  spyOn(window, 'alert');
+});
+
+describe("Check String tests", function() {
+
+  it("should get rid of the letters in a string",function () {
+    expect(CheckString("$2341sdfa23sdf22sd.1234ssdf")).toEqual(23412322.1234);
+  });
+
+it("should return a number",function () {
+  expect(CheckString("pie3.14")).toEqual(jasmine.any(Number));
+});
+
+
+it("shouldn't break if there are no numers in the string",function () {
+  expect(CheckString("CatsAreTheBest")).toEqual(0);
+});
+
+it("should Alert the user if there are no numbers",function () {
+  CheckString("CatsAreTheBest");
+  expect(window.alert).toHaveBeenCalledWith('Please put in a Number!');
+});
+
+it("should still work with spaces",function () {
+  expect(CheckString("$2341s dfa23sd   f22sd.1234 ssdf")).toEqual(23412322.1234);
+});
+
+
+});
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,11 @@
+let monthlyPayment;
+let loanAmount;
+let loanYears;
+let loanRate;
+
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
@@ -9,32 +17,94 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+
+
+
+
 function getCurrentUIValues() {
+//I got rid of the + because if someone decides to put in a $ or a letter with the number then it would break
+//it would just give you NaN
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
+    amount: (loanAmount.value),
+    years: (loanYears.value),
+    rate: (loanRate.value),
   }
 }
 
-// Get the inputs from the DOM.
+
+
+function setupIntialValues()
+{
+  // Get the inputs from the DOM.
+loanAmount = document.getElementById("loan-amount");
+loanYears = document.getElementById("loan-years");
+ loanRate = document.getElementById("loan-rate");
+
 // Put some default values in the inputs
+loanAmount.value = "$2000";
+loanYears.value = "20yr";
+loanRate.value = "10";
+
+
 // Call a function to calculate the current monthly payment
-function setupIntialValues() {
+update();
 }
 
-// Get the current values from the UI
-// Update the monthly payment
+
 function update() {
+  // Get the current values from the UI
+  let inputs = getCurrentUIValues();
+  monthlyPayment = calculateMonthlyPayment(inputs);
+
+  // Update the monthly payment
+updateMonthly(monthlyPayment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const p = CheckString(values['amount']);
+  let yr = CheckString(values['years']);
+  let rt = CheckString(values['rate']);
+
+let i =((rt/100)/12);
+let n =Math.floor(yr*12);
+
+let mp = (p*i)/(1-(Math.pow((1+i),-n)));
+
+mp = mp.toFixed(2);
+return mp;
 }
+
+
+  //this is so that if the user puts any leters or symbols like $, it won't break
+function CheckString(str){
+let newStr ="";
+let regex =/[\d,.]/;
+
+for(let s of str)
+{
+if(regex.test(s)){
+  newStr += s;
+}
+}
+if(newStr ===""){
+  alert("Please put in a Number!");
+  return 0;
+}
+
+return parseFloat(newStr);
+}
+
+
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {
+function updateMonthly(monthly){
+
+let paymentTxt =document.getElementById("monthly-payment");
+
+paymentTxt.innerText = "$"+monthly;
+
 }
